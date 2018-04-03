a3fcb439cac1ade1cb5d7d66aa014b04e3c49b31
Ronald Bow <ronald.bow@hootsuite.com>
2018-04-03 15:26:20 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,111 @@
+# maintenance-calendar
+> generate AWS maintenance calendar
+
+Generates an iCal calendar with information about scheduled maintenance events retrieved from AWS Personal Health Dashboard
+
+Uploads calendar to S3
+
+This allows us to subscribe and view scheduled events from within Google Calendar for example.
+
+## Installation
+
+OS X & Linux:
+
+```sh
+go build main.go
+```
+
+Set up AWS IAM policy with the following permissions. 
+
+```
+{
+    "Version": "2012-10-17",
+    "Statement": [
+         {
+            "Action": [
+                "s3:ListAllMyBuckets"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "arn:aws:s3:::*"
+            ]
+        },
+        {
+            "Action": [
+                "s3:ListBucket",
+                "s3:ListBucketMultipartUploads",
+                "s3:ListBucketVersions",
+                "s3:ListMultipartUploadParts",
+                "s3:GetBucketAcl"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "arn:aws:s3:::BUCKETNAME"
+            ]
+        },
+        {
+            "Sid": "Stmt1407264241000",
+            "Effect": "Allow",
+            "Action": [
+                "s3:Get*",
+                "s3:List*",
+                "s3:Put*"
+            ],
+            "Resource": [
+                "arn:aws:s3:::BUCKETNAME{/PREFIX}",
+                "arn:aws:s3:::BUCKETNAME{/PREFIX}/*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "health:Describe*",
+                "ec2:Describe*",
+                "rds:Describe*"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+```
+
+Setup AWS credentials as needed via `aws configure` or through an instance role policy
+
+## Usage example
+
+```sh
+./main --bucket-region=us-east-1 --bucket=bucketname --prefix=maintenancecalendar --filename=webcal.ics
+```
+
+This would generate a local `webcal.ics` file and attempt to upload to S3 with the URL `https://s3.amazonaws.com/bucket/maintenancecalendar/webcal.ics`
+
+The ICS URL can then be used in your local calendar client or Google Calendar. Slack can be authorized to read from Google Calendar for useful chat notification.
+
+## Development setup
+
+```sh
+go get
+```
+
+## Release History
+
+* 0.0.1
+    * initial release
+
+## Meta
+
+Ronald Bow – [@rmkbow](https://twitter.com/rmkbow) – ronald.bow@hootsuite.com - [https://github.com/rmkbow](https://github.com/rmkbow) 
+
+Distributed under the Apache license. See ``LICENSE`` for more information.
+
+[https://github.com/hootsuite/maintenance-calendar](https://github.com/hootsuite/maintenance-calendar)
+
+## Contributing
+
+1. Fork it (<https://github.com/yourname/yourproject/fork>)
+2. Create your feature branch (`git checkout -b feature/fooBar`)
+3. Commit your changes (`git commit -am 'Add some fooBar'`)
+4. Push to the branch (`git push origin feature/fooBar`)
+5. Create a new Pull Request
+
+
---
+++ main.go
@@ -0,0 +1,365 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"strconv"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/health"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/rds"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/rmkbow/ical-go"
+	"net/url"
+	"os"
+	"strings"
+	"flag"
+)
+
+var aws_session *session.Session
+var health_connection *health.Health
+var ec2_connection map[string]*ec2.EC2
+var rds_connection map[string]*rds.RDS
+
+func error_check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	bucket_region := flag.String("bucket-region", "", "Region of the S3 bucket")
+	bucket := flag.String("bucket", "", "Name of the S3 bucket")
+	prefix := flag.String("prefix", "/", "Prefix of the S3 path (optional)")
+	filename := flag.String("filename", "", "Filename of the local and destination file")
+	flag.Parse()
+	exit_flag := 0
+
+	if *bucket_region == "" {
+		exit_flag = 2
+		fmt.Println("--bucket-region REGION")
+	}
+	if *bucket == "" {
+		exit_flag = 2
+		fmt.Println("--bucket BUCKETNAME")
+	}
+	if *filename == "" {
+		exit_flag = 2
+		fmt.Println("--filename FILENAME")
+	}
+
+	if exit_flag != 0 {
+		os.Exit(exit_flag)
+	}
+
+	initialize()
+	calendar := calendar(calendar_events(health_events()))
+	save_calendar_to_file(*filename, calendar)
+	upload_file(*bucket_region, *bucket, *prefix, *filename)
+}
+
+func initialize() {
+	aws_session, _ = session.NewSession()
+	ec2_connection = make(map[string]*ec2.EC2)
+	health_connection = health.New(aws_session, &aws.Config{Region: aws.String("us-east-1")})
+	rds_connection = make(map[string]*rds.RDS)
+}
+
+func initialize_ec2_connection(region string) {
+	ec2_connection[region] = ec2.New(aws_session, &aws.Config{Region: aws.String(region)})
+}
+
+func initialize_rds_connection(region string) {
+	rds_connection[region] = rds.New(aws_session, &aws.Config{Region: aws.String(region)})
+}
+
+func health_events() []*health.Event {
+	describe_event_filter := &health.EventFilter{
+		EventTypeCategories: []*string{aws.String("scheduledChange")},
+		EventStatusCodes: []*string{aws.String("open"), aws.String("upcoming")},
+	}
+	describe_event_params := &health.DescribeEventsInput{
+		Filter: describe_event_filter,
+	}
+	describe_event_params.SetMaxResults(100)
+	health_events,_ := health_connection.DescribeEvents(describe_event_params)
+	return health_events.Events
+}
+
+func resource_ids(health_arn *string) []string {
+	describe_affected_entities_params := &health.DescribeAffectedEntitiesInput{
+		Filter: &health.EntityFilter{
+			EventArns: []*string{health_arn},
+		},
+	}
+	describe_affected_entities_params.SetMaxResults(100)
+	var resource_ids []string
+	affected_entities, _ := health_connection.DescribeAffectedEntities(describe_affected_entities_params)
+	for _, entity := range affected_entities.Entities {
+		resource_ids = append(resource_ids,*entity.EntityValue)
+	}
+	return resource_ids
+}
+
+func process_event(health_arn *string) ([]string, string, string, string, *time.Time, *time.Time) {
+
+	resource_ids := resource_ids(health_arn)
+
+	describe_event_params := &health.DescribeEventDetailsInput{
+		EventArns: []*string{health_arn},
+	}
+	detailed_events, _ := health_connection.DescribeEventDetails(describe_event_params)
+	var description string
+	var event_type string
+	var event_service string
+	var event_start_time *time.Time
+	var event_end_time *time.Time
+	for _, set := range detailed_events.SuccessfulSet {
+		description = *set.EventDescription.LatestDescription
+		event_type = *set.Event.EventTypeCode
+		event_service = *set.Event.Service
+		event_start_time = set.Event.StartTime
+		event_end_time = set.Event.EndTime
+	}
+	return resource_ids, description, event_type, event_service, event_start_time, event_end_time
+}
+
+func ec2_instance_name(instance_id string, region string) string {
+	params := &ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{
+			{
+				Name: aws.String("instance-id"),
+				Values: []*string{
+					aws.String(instance_id),
+				},
+			},
+		},
+	}
+
+	var ec2din_response *ec2.DescribeInstancesOutput
+	var err error
+	if ec2_connection[region] == nil {
+		initialize_ec2_connection(region)
+	}
+	ec2din_response, err = ec2_connection[region].DescribeInstances(params)
+	error_check(err)
+
+	var instance_name string
+	for _, reservations := range ec2din_response.Reservations {
+		for _, instance := range reservations.Instances {
+			for _, tag := range instance.Tags {
+				if *tag.Key == "Name" {
+					instance_name = url.QueryEscape(*tag.Value)
+				}
+			}
+		}
+	}
+	return instance_name
+}
+
+
+func rds_maintenance_window(name string, region string) string {
+	var rds_maintenance_window string
+
+	if rds_connection[region] == nil {
+		initialize_rds_connection(region)
+	}
+
+	describe_rds_cluster_filter := &rds.DescribeDBClustersInput{
+		DBClusterIdentifier: aws.String(name),
+	}
+	db_clusters,_ := rds_connection[region].DescribeDBClusters(describe_rds_cluster_filter)
+	for _,db_cluster := range db_clusters.DBClusters {
+		rds_maintenance_window = *db_cluster.PreferredMaintenanceWindow
+	}
+
+	if rds_maintenance_window != "" {
+		return rds_maintenance_window
+	}
+
+	describe_rds_instance_filter := &rds.DescribeDBInstancesInput{
+		DBInstanceIdentifier: aws.String(name),
+	}
+	db_instances,_ := rds_connection[region].DescribeDBInstances(describe_rds_instance_filter)
+	for _,db_instance := range db_instances.DBInstances {
+		rds_maintenance_window = *db_instance.PreferredMaintenanceWindow
+	}
+
+	return rds_maintenance_window
+}
+
+
+func calendar(calendar_events []ical.CalendarEvent) ical.Calendar {
+	calendar := ical.Calendar{calendar_events}
+	return calendar
+}
+
+func save_calendar_to_file(filename string, calendar ical.Calendar) {
+	f, _ := os.Create(filename)
+	defer f.Close()
+	fmt.Fprintf(f, calendar.ToICS())
+}
+
+func calendar_event(id string, summary string, description string, location string, start_time *time.Time, end_time *time.Time) ical.CalendarEvent {
+	calendar_event := ical.CalendarEvent{
+		Id: id,
+		Summary: summary,
+		Description: description,
+		Location: location,
+		URL: "https://phd.aws.amazon.com/phd/home?region=us-east-1#/dashboard/scheduled-changes",
+		StartAt: start_time,
+		EndAt: end_time,
+	}
+	return calendar_event
+}
+
+func calendar_events(health_events []*health.Event) []ical.CalendarEvent {
+	var calendar_events []ical.CalendarEvent
+	for _, health_event := range health_events {
+		health_arn := health_event.Arn
+		event_affected_resources, event_description, full_event_type, event_service, event_start_time, event_end_time := process_event(health_arn)
+
+		event_type := full_event_type
+
+		switch full_event_type {
+		case "AWS_EC2_INSTANCE_REBOOT_MAINTENANCE_SCHEDULED":
+			event_type = "REBOOT"
+		case "AWS_EC2_INSTANCE_POWER_MAINTENANCE_SCHEDULED":
+			event_type = "REBOOT"
+		case "AWS_EC2_INSTANCE_RETIREMENT_SCHEDULED":
+			event_type = "RETIREMENT"
+		case "AWS_EC2_INSTANCE_NETWORK_MAINTENANCE_SCHEDULED":
+			event_type = "NET MAINT"
+		case "AWS_RDS_MAINTENANCE_SCHEDULED":
+			event_type = "MAINT SCHEDULED"
+		}
+
+		calendar_event_id := *health_arn
+		calendar_event_description := event_description
+		calendar_event_start_time := event_start_time
+		calendar_event_end_time := event_end_time
+		calendar_event_location := *health_event.Region
+
+		for _, event_affected_resource := range event_affected_resources {
+			var calendar_event_summary string
+			switch event_service {
+			case "EC2":
+				calendar_event_summary = event_type + " " + ec2_instance_name(event_affected_resource, *health_event.Region) + " " + event_affected_resource
+			case "RDS":
+				calendar_event_summary = event_service + " " + event_affected_resource + " " + event_type
+				calendar_event_start_time_rds, calendar_event_end_time_rds := rds_maintenance_time(calendar_event_start_time, rds_maintenance_window(event_affected_resource, *health_event.Region))
+				calendar_event_start_time = &calendar_event_start_time_rds
+				calendar_event_end_time = &calendar_event_end_time_rds
+			default:
+				calendar_event_summary = event_service + " " + event_affected_resource + " " + event_type
+			}
+			calendar_event := calendar_event(calendar_event_id + "_" + event_affected_resource, calendar_event_summary, calendar_event_description, calendar_event_location, calendar_event_start_time, calendar_event_end_time)
+			calendar_events = append(calendar_events, calendar_event)
+		}
+	}
+	return calendar_events
+}
+
+func rds_maintenance_time(event_start *time.Time, maintenance_window string) (time.Time, time.Time) {
+	//parsing maintenance time
+	//maintenance_window is in string format ddd:hh24:mi-ddd:hh24:mi
+	maintenance_window_start_end := strings.Split(maintenance_window, "-")
+	maintenance_window_start := strings.Split(maintenance_window_start_end[0], ":")
+	maintenance_window_end := strings.Split(maintenance_window_start_end[1], ":")
+	// maintenance_window_start[0] for weekday
+	// maintenance_window_start[1] for hour
+	// maintenance_window_start[2] for minute
+
+	maintenance_window_start_time, maintenance_window_end_time := next_maintenance_window(event_start, maintenance_window_start, maintenance_window_end)
+
+	return maintenance_window_start_time, maintenance_window_end_time
+}
+
+func next_maintenance_window(base_time *time.Time, maintenance_window_start []string, maintenance_window_end []string) (time.Time, time.Time) {
+	maintenance_window_start_weekday := weekday_from_shortname(maintenance_window_start[0])
+	maintenance_window_start_hour_int64,_ := strconv.ParseInt(maintenance_window_start[1], 10, 8)
+	maintenance_window_start_hour := int(maintenance_window_start_hour_int64)
+	maintenance_window_start_minute_int64,_ := strconv.ParseInt(maintenance_window_start[2], 10, 8)
+	maintenance_window_start_minute := int(maintenance_window_start_minute_int64)
+
+	maintenance_window_end_weekday := weekday_from_shortname(maintenance_window_end[0])
+	maintenance_window_end_hour_int64,_ := strconv.ParseInt(maintenance_window_end[1], 10, 8)
+	maintenance_window_end_hour := int(maintenance_window_end_hour_int64)
+	maintenance_window_end_minute_int64,_ := strconv.ParseInt(maintenance_window_end[2], 10, 8)
+	maintenance_window_end_minute := int(maintenance_window_end_minute_int64)
+
+	days_ahead := maintenance_window_start_weekday - base_time.Weekday()
+	if days_ahead < 0 {
+		days_ahead += 7
+	} else if days_ahead == 0 && maintenance_window_start_hour < base_time.Hour() && maintenance_window_start_minute < base_time.Minute(){
+		days_ahead += 7
+	}
+	next_date := base_time.AddDate(0,0,int(days_ahead))
+	next_maintenance_window_start := time.Date(next_date.Year(), next_date.Month(), next_date.Day(), maintenance_window_start_hour, maintenance_window_start_minute, 0, 0, next_date.Location())
+
+	next_maintenance_window_end := next_maintenance_window_start
+	if maintenance_window_end_weekday != maintenance_window_start_weekday {
+		next_maintenance_window_end = next_maintenance_window_end.AddDate(0,0,int(1))
+	}
+	next_maintenance_window_end = time.Date(next_maintenance_window_end.Year(), next_maintenance_window_end.Month(), next_maintenance_window_end.Day(), maintenance_window_end_hour, maintenance_window_end_minute, 0, 0, next_date.Location())
+	return next_maintenance_window_start, next_maintenance_window_end
+}
+
+func weekday_from_shortname(shortname string) time.Weekday {
+	var weekday time.Weekday
+	switch shortname {
+	case "sun":
+		weekday = time.Weekday(0)
+	case "mon":
+		weekday = time.Weekday(1)
+	case "tue":
+		weekday = time.Weekday(2)
+	case "wed":
+		weekday = time.Weekday(3)
+	case "thu":
+		weekday = time.Weekday(4)
+	case "fri":
+		weekday = time.Weekday(5)
+	case "sat":
+		weekday = time.Weekday(6)
+	}
+	return weekday
+}
+
+
+func upload_file(bucket_region string, bucket string, prefix string, filename string) {
+	if !strings.HasPrefix(prefix, "/") {
+		prefix = "/" + prefix
+	}
+	if !strings.HasSuffix(prefix, "/") {
+		prefix = prefix + "/"
+	}
+
+	file, open_err := os.Open(filename)
+	if open_err != nil {
+		error_check(open_err)
+	}
+	defer file.Close()
+	sess, _ := session.NewSession(&aws.Config{Region: aws.String(bucket_region)})
+	uploader := s3manager.NewUploader(sess)
+
+	_, upload_err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket),
+		Key: aws.String(prefix + filename),
+		Body: file,
+		ACL: aws.String("public-read"),
+	})
+	if upload_err != nil {
+		error_check(upload_err)
+	}
+
+	cal_url := ""
+	if bucket_region == "us-east-1" {
+		cal_url = "https://s3.amazonaws.com/" + bucket + prefix + filename;
+	} else {
+		cal_url = "https://s3-" + bucket_region + ".amazonaws.com/" + bucket + prefix + filename;
+	}
+
+	fmt.Printf("Successfully uploaded to %q\n", cal_url)
+}
